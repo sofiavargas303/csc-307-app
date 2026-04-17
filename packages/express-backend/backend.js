39649924254acb41f7d94ae4feae44fd3d428b0a
sofiavargas303 <sofifiswim@gmail.com>
2026-04-17 10:31:26 -0700

    Finish express backend assignment

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,10 +5,119 @@ const port = 8000;
 
 app.use(express.json());
 
+<<<<<<< HEAD
+=======
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+const findUserByName = (name) => {
+  return users.users_list.filter((user) => user.name === name);
+};
+
+const findUserById = (id) => {
+  return users.users_list.find((user) => user.id === id);
+};
+
+const addUser = (user) => {
+  users.users_list.push(user);
+  return user;
+};
+
+const deleteUserById = (id) => {
+  const index = users.users_list.findIndex((user) => user.id === id);
+
+  if (index === -1) {
+    return false;
+  }
+
+  users.users_list.splice(index, 1);
+  return true;
+};
+
+const findUserByNameAndJob = (name, job) => {
+  return users.users_list.filter(
+    (user) => user.name === name && user.job === job
+  );
+};
+
+>>>>>>> 4bf5bdd (Finish express backend assignment)
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+<<<<<<< HEAD
+=======
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+
+  if (name && job) {
+    const result = findUserByNameAndJob(name, job);
+    res.send({ users_list: result });
+  } else if (name) {
+    const result = findUserByName(name);
+    res.send({ users_list: result });
+  } else {
+    res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const result = findUserById(id);
+
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deleted = deleteUserById(id);
+
+  if (!deleted) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send();
+  }
+});
+
+>>>>>>> 4bf5bdd (Finish express backend assignment)
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
