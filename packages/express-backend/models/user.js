d1d86d95e030649f282a0a230fea1f3e30e7d87d
Sofia Vargas <149122991+sofiavargas303@users.noreply.github.com>
2026-04-27 10:13:50 -0700

    Update user.js

--- packages/express-backend/models/user.js
+++ packages/express-backend/models/user.js
@@ -1,17 +1,10 @@
-{
-  "name": "express-backend",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "start": "node backend.js",
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "type": "module",
-  "dependencies": {
-    "cors": "^2.8.6"
-  }
-}
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  job: String
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
